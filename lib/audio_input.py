18933a65e16ac5f592f3c92085ea496667f3988d
Marc Baetica <marc.baetica@gmail.com>
2022-01-19 21:56:58 -0500

    Plotting wave on graph.

--- lib/audio_input.py
+++ lib/audio_input.py
@@ -1,5 +1,6 @@
 import pyaudio
 import wave
+from bokeh.plotting import figure, show
 
 
 port_audio = pyaudio.PyAudio()
@@ -24,3 +25,12 @@ with wave.open('recording.wav', 'wb') as wave_writer:
     wave_writer.setsampwidth(port_audio.get_sample_size(pyaudio.paInt24))
     wave_writer.setframerate(44100)
     wave_writer.writeframes(b''.join(frames))
+
+
+y = list(range(len(frames)))
+# print(frames[2])
+# print(frames[2].decode('latin-1'))
+
+p = figure()
+p.vbar(x=frames, top=y, width=0.8)
+show(p)  # Ignored in bokeh serve call. Will only render in run. Will not render if saving to file is called.
