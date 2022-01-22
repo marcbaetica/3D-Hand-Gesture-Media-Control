e3cd3c4c992156fca6bd83c8781c184bddd0e408
Marc Baetica <marc.baetica@gmail.com>
2022-01-21 19:10:40 -0500

    Env variables and some refactoring.

---
+++ lib/audio_processing.py
@@ -0,0 +1,39 @@
+import pyaudio
+import wave
+from bokeh.plotting import figure, show
+
+
+def record_audio(sample_frequency, bit_rate, audio_channels):
+    port_audio = pyaudio.PyAudio()
+
+    stream = port_audio.open(rate=sample_frequency,
+                             channels=audio_channels,
+                             format=pyaudio.paInt16,
+                             input=True,
+                             frames_per_buffer=1024)
+    print('Recording!')
+
+    frames = []
+
+    for i in range(int(44100/1024*2)):
+        frames.append(stream.read(1024))
+
+    print('Done recording!')
+    port_audio.terminate()
+
+
+def save_recording(frames, port_audio, sample_frequency, bit_rate, audio_channels):
+    with wave.open('recording_16bit.wav', 'wb') as wave_writer:
+        wave_writer.setsampwidth(port_audio.get_sample_size(pyaudio.paInt16))
+        wave_writer.setframerate(sample_frequency)
+        wave_writer.setnchannels(audio_channels)
+        wave_writer.writeframes(b''.join(frames))
+
+
+# y = list(range(len(frames)))
+# print(frames[2])
+# print(frames[2].decode('latin-1'))
+#
+# p = figure()
+# p.vbar(x=frames, top=y, width=0.8)
+# show(p)  # Ignored in bokeh serve call. Will only render in run. Will not render if saving to file is called.
--- main.py
+++ main.py
@@ -1,1 +1,18 @@
-print('Test!')
+import os
+import sys
+from lib.audio_processing import record_audio
+from dotenv import load_dotenv
+
+
+
+load_dotenv()
+
+try:
+    SAMPLE_FREQUENCY = os.environ['SAMPLE_FREQUENCY']
+    BIT_DEPTH = os.environ['BIT_DEPTH']
+    AUDIO_CHANNELS = os.environ['AUDIO_CHANNELS']
+except KeyError as e:
+    print(f'ERROR: Could not load environment variable [{e}]. Please check the .env file definitions.')
+    sys.exit(1)
+
+record_audio(SAMPLE_FREQUENCY, BIT_DEPTH, AUDIO_CHANNELS)
