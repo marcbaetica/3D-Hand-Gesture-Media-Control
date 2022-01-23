7bc08a01112d720454e6601689657c1225867b10
Marc Baetica <marc.baetica@gmail.com>
2022-01-23 12:44:22 -0500

    Historic audio time domain frames capture.

--- digital_assistant.py
+++ digital_assistant.py
@@ -2,6 +2,7 @@ import os
 import sys
 from dotenv import load_dotenv
 from pyaudio import PyAudio
+from time import perf_counter
 from threading import Thread
 
 
@@ -24,19 +25,28 @@ class DigitalAssistant:
         print(f'Loading digital assistant: {name}')
         self.name = name
         self.port_audio = PyAudio()
-        self.listening_stream = Thread(target=self.open_listening_stream_on_port_audio())
+        self.microphone_frames = []
+        self.listening_stream = self.open_listening_stream_on_port_audio()
+        self.capture_stream_data()  # TODO: thread it.
         self.response_stream = None
 
     def open_listening_stream_on_port_audio(self):
-        listening_stream = self.port_audio.open(
+        return self.port_audio.open(
                                     rate=SAMPLE_FREQUENCY,
                                     channels=AUDIO_CHANNELS,
                                     format=self.port_audio.get_format_from_width(int(BIT_DEPTH/8)),
                                     input=True,
-                                    frames_per_buffer=1024
+                                    frames_per_buffer=1024  # wha?
                                 )
-        for _ in range(20):
-            frame = listening_stream.read(1024)
-            # print(frame, len(frame))
-            # print(type(frame))
-            print([int(item, base=16) for item in frame.hex('-', bytes_per_sep=2).split('-')])
+
+    def capture_stream_data(self):
+        print(perf_counter())
+        for _ in range(41):
+            print(f'Before reading frames: {len(self.microphone_frames)}')
+            frames = self.listening_stream.read(int(SAMPLE_FREQUENCY/4))
+            self.microphone_frames += [int(item, base=16) for item in frames.hex('-', bytes_per_sep=2).split('-')]
+            print(f'After reading frames: {len(self.microphone_frames)}')
+            if len(self.microphone_frames) > int(SAMPLE_FREQUENCY*3):
+                self.microphone_frames = self.microphone_frames[int(SAMPLE_FREQUENCY/4):]
+            print(f'After cleaning frames: {len(self.microphone_frames)}')
+        print(perf_counter())
