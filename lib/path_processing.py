a883c1d3715bec94d0c7faa2f49cc52e9ea1f037
Marc Baetica <marc.baetica@gmail.com>
2022-01-31 14:58:39 -0500

    Added path conversion tests, convert string path to absolute path.

--- lib/path_processing.py
+++ lib/path_processing.py
@@ -3,7 +3,7 @@ from pathlib import Path
 
 def convert_to_string_if_path_obj(obj):
     if isinstance(obj, str):
-        return obj
+        return str(Path(obj).absolute())
     elif isinstance(obj, Path):
         return str(obj.absolute())
     else:
---
+++ run_tests.py
@@ -0,0 +1,4 @@
+import subprocess
+
+
+subprocess.run(['python', '-m', 'unittest', 'discover'])
